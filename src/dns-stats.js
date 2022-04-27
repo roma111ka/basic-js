9da8aeb6f4c952c3c2a1ce95311089f6f72167f7
Роман Лодяты Александрович <68326671+roma111ka@users.noreply.github.com>
2022-04-27 23:34:45 +0300

    feat: add dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats( domains ) {
+  let Obj = {};
+  if (domains.length == 0) {
+    return Obj;    
+  }
+  domains.forEach(domain =>{
+    let words = domain.split('.').reverse();
+    let key = '';
+    for (let word of words) {
+      key += `.${word}`;
+      !Obj[key] ? Obj[key] = 1 : Obj[key]++;
+    }
+  });
+  
+  return answer;
 }
 
 module.exports = {
